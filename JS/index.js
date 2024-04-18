aa4e6934712d2f340d039fa0eced075e1bdac841
Abega1642 <151494226+Abega1642@users.noreply.github.com>
2024-04-18 22:45:56 +0330

    feat: functionalities ameliorations of login page

--- JS/index.js
+++ JS/index.js
@@ -1,14 +1,35 @@
-const password = 'razafindrateloAbegà';
+const password = 'razafindratelo';
 const button = document.querySelector('button');
 const input = document.getElementById('pswd');
 const email = 'a.razafindratelo@gmail.com';
 const mail = document.getElementById('eMail');
+const message = document.getElementById('alert')
 
 input.addEventListener('keydown', function(e) {
     if(e.key === "Enter") {
-        button.innerHTML = ". . . ."
+        if (input.value != password) {
+            input.value = '';
+            mail.value = '';
+            message.classList.toggle('hidden')
+            button.innerText = ". . . ."
+            setTimeout(() => {
+                button.innerText = "Login Now"
+                setTimeout(() => {
+                    console.log('SQDF');
+                })
+                message.classList.toggle('hidden')
+            },2000)
+        }   
     }
 })
 button.addEventListener('click', () => {
     button.innerHTML = ". . . ."
+    if (input.value != password) {
+        input.value = '';
+        mail.value = '';
+        message.classList.toggle('hidden')
+        setTimeout(() => {
+            message.classList.toggle('hidden')
+        },2000)
+    }
 })
