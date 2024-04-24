a50ee90db85641437544cdb973de941738b94ef8
Abega1642 <151494226+Abega1642@users.noreply.github.com>
2024-04-24 19:58:57 +0330

    fix: main page - second section - counter bugs fixed

--- JS/main.js
+++ JS/main.js
@@ -4,14 +4,8 @@ const frequency = document.getElementById('satisfactionNumber');
 const countryNumber = document.getElementById('countriesNumber');
 const experiences = document.getElementById('experiencesYear');
 
-
-window.addEventListener('scroll', () => {
-    if(document.documentElement.scrollTop > 15) {
-        navbar.className = 'navbar-moove'
-    } else {
-        navbar.className = 'navbar-moove-back';
-    }
-    if(document.documentElement.scrollTop > 160) {
+function counter(first) {
+    if (first == false) {
         let incr = 0;
         let country = 0;
         let experiencesYear = 0;
@@ -20,27 +14,44 @@ window.addEventListener('scroll', () => {
         firstSection.classList.add('show');
         let frequencyInterval = setInterval(() => {
             frequency.innerText = `${incr}%`
-            incr++;
-            if (incr === 100) {
+            if (incr == 99) {
                 clearInterval(frequencyInterval);
             }
+            incr += 1;
         }, 90);
 
         let countryInterval = setInterval(() => {
             countryNumber.innerText = `${country}`;
-            country++;
-            if(country == 221) {
+            if (country == 220) {
                 clearInterval(countryInterval)
             }
+            country += 1;
         }, 40);
 
         let experiencesYearInterval = setInterval(() => {
             experiences.innerText = `${experiencesYear}`;
-            experiencesYear++;
-            if (experiencesYear == 51) {
+            if (experiencesYear == 52) {
                 clearInterval(experiencesYearInterval)
             }
+            experiencesYear += 2;
         }, 190)
-        
+    }
+}
+
+let first = false;
+
+window.addEventListener('scroll', () => {
+    if (document.documentElement.scrollTop > 15) {
+        navbar.className = 'navbar-moove'
+    } else {
+        navbar.className = 'navbar-moove-back';
+    }
+    if (document.documentElement.scrollTop >= 200 && first == false) {
+        counter(first);
+        first = true;
+    } else {
+        frequency.innerText = '99%';
+        countryNumber.innerText = '220';
+        experiences.innerText = '50';
     }
 })
